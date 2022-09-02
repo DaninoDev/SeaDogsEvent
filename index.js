b58bfe3fb940b29113afc7e3a107d1cb2171cbce
Daniel Lazzaroni <88000948+Daninogamer@users.noreply.github.com>
2022-09-02 11:49:24 +0200

    fix

--- index.js
+++ index.js
@@ -87,56 +87,6 @@ tempChannels.on('childCreate', async (member, channel) => { // ?Editing
 		}
 });
 
-//reaction role
-
-client.on("message", message => {
-	if (message.content == "$lamignottadellamadonndsadsadasdsadsadasdasdasdasdsaa") {
-		const embed = new MessageEmbed() //Crea il tuo embed o messaggio normale
-		.setColor("FUCHSIA")
-        .setDescription('Welcome to the Sea Dogs Events!\n\n To start competing in our events we suggest you to register your team by filling the form in <#999231975929946163>.\nIf you want to join one, you can look who is recruiting in <#1003602058013855824> or make you known in <#1003588935752101969>.\nFor now you can compete in these 2 activities that we created for now but will be added more in the future:\n\nTDM: a fight on an isle with only a sniper and a pistol where the better one wins\n\nBattle Royale: a full naval fight experience.\n\n**React to the emoji 💥 to get the role <@&999407962982465536>\nReact to the emoji 🚢 to get the role <@&999407195135426600>**\n\n *By reacting underneath u agree to the rules, and granting you access to the server.*')
-		
-        message.channel.send({ embeds: [embed] })
-		.then(msg => {
-			//Inserire tutte le reazioni che si vogliono
-			msg.react("💥")
-			msg.react("🚢")
-		})
-    }
-})
-//Quando viene cliccata una reazione
-client.on("messageReactionAdd", async function (messageReaction, user) {
-	if (user.bot) return //Le reaction dei bot verranno escluse
-	
-    if (messageReaction.message.partial) await messageReaction.message.fetch();
-	
-    if (messageReaction.message.id == "1008305540453126184") { //Settare id messaggio
-        if (messageReaction._emoji.name == "💥") { 
-			var utente = messageReaction.message.guild.members.cache.find(x => x.id == user.id);
-            utente.roles.add("999407962982465536"); //Settare ruolo
-        }
-        if (messageReaction._emoji.name == "🚢") { 
-			var utente = messageReaction.message.guild.members.cache.find(x => x.id == user.id);
-            utente.roles.add("999407195135426600");
-        }
-    }
-})
-//Quando viene rimossa una reazione
-client.on("messageReactionRemove", async function (messageReaction, user) {
-	if (user.bot) return
-	
-    if (messageReaction.message.partial) await messageReaction.message.fetch();
-	
-    if (messageReaction.message.id == "1008305540453126184") {
-		if (messageReaction._emoji.name == "💥") {
-			var utente = messageReaction.message.guild.members.cache.find(x => x.id == user.id);
-            utente.roles.remove("999407962982465536");
-        }
-        if (messageReaction._emoji.name == "🚢") {
-			var utente = messageReaction.message.guild.members.cache.find(x => x.id == user.id);
-            utente.roles.remove("999407195135426600");
-        }
-    }
-})
 
 const memberadd = require("./events/MemberAdd");
 memberadd(client);
@@ -166,86 +116,12 @@ client.on("guildMemberRemove", (member) => {
 	channel1.send({ embeds: [embed] })
 })
 
-//transcript
-const Discord = require('discord.js');
 
-client.on("messageCreate", async message => {
-	if (message.content == ">transcript") {
-		let chatLog = `-- TICKET TRANSCRIPT #${message.channel.name} --\n\n`
-		
-        let messages = await getAllMessages(message.channel)
-        messages.reverse().forEach(msg => {
-			chatLog += `@${msg.author.tag} ID: ${msg.author.id} - ${msg.createdAt.toLocaleString()}\n`
-			
-            if (msg.content) chatLog += `${msg.content}\n`
-			
-            if (msg.embeds[0]) {
-				chatLog += `Embed:\n`
-                if (msg.embeds[0].title) chatLog += `Title: ${msg.embeds[0].title}\n`
-                if (msg.embeds[0].description) chatLog += `Description: ${msg.embeds[0].description}\n`
-                if (msg.embeds[0].fields[0]) chatLog += `Fields: ${msg.embeds[0].fields.map(x => `${x.name}-${x.value}`).join(", ")}\n`
-            }
-			
-            if (msg.attachments.size > 0)
-			chatLog += `Files: ${msg.attachments.map(x => `${x.name} (${x.url})`).join(", ")}\n`
-			
-            if (msg.stickers.size > 0)
-			chatLog += `Stickers: ${msg.stickers.map(x => `${x.name} (${x.url})`).join(", ")}\n`
-
-            chatLog += "\n"
-        })
-		
-        let attachment = new Discord.MessageAttachment(Buffer.from(chatLog, "utf-8"), `chatLog-channel-${message.channel.id}.txt`)
-		
-        const modmail = await ModmailList.findOne({ Channel_ID: message.channel.id });
-		if (!modmail) return message.channel.send({ content: '**Please use this command in a channel ticket valide!**', ephemeral: true });
-        let member = message.guild.members.cache.get(modmail.User_ID);
-        let embed = new Discord.MessageEmbed()
-		.setTitle("Ticket Transcript")
-		.setDescription(`here is the transcript of the ticket of ${member}`)
-		.setColor('GOLD')
-		.setTimestamp()
-		const tickettranscript = message.guild.channels.cache.get('1003626545308500018')
-        tickettranscript.send({ embeds: [embed], files: [attachment] })
-    }
-})
-
-
-
-const getAllMessages = async (channel) => {
-	let allMessages = []
-    let lastMessage
-	
-    while (true) {
-		const options = { limit: 100 }
-        if (lastMessage) options.before = lastMessage
-		
-        let messages = await channel.messages.fetch(options)
-		
-        allMessages = allMessages.concat(Array.from(messages.values()))
-		
-        lastMessage = messages.last().id
-		
-        if (messages.size != 100) {
-			break
-        }
-    }
-	
-    return allMessages
-}
-
-//clock
-
-//'ready' event
 client.once('ready', () => {
-	//init time
-	const timeNow = moment().tz(Timezone).format(Format);
-	//define clockChannel
-  const clockChannel = client.channels.cache.get(ChannelClockID);
-  //initial update
+const timeNow = moment().tz(Timezone).format(Format);
+ const clockChannel = client.channels.cache.get(ChannelClockID);
   clockChannel.edit({ name: `🕒 ${timeNow}` }, 'Clock update')
     .catch(console.error);
-  //set the interval
   setInterval(() => {
     const timeNowUpdate = moment().tz(Timezone).format(Format);
     clockChannel.edit({ name: `🕒 ${timeNowUpdate}` }, 'Clock update')
